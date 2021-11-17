9d09580a6a012a2a277fcbea58426881b8c09fba
Osttanya <ostania.tania@gmail.com>
2021-11-17 20:41:32 +0200

    add factorial

--- src/main/java/stepik/Factorial.java
+++ src/main/java/stepik/Factorial.java
@@ -1,4 +1,15 @@
 package stepik;
 
+import java.math.BigInteger;
+
+//факториал числа n — это произведение всех натуральных чисел от 1 до n включительно
+
 public class Factorial {
+    public static BigInteger factorial(int value) {
+        return value == 1 ? BigInteger.ONE : BigInteger.valueOf(value).multiply(factorial(value - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(1));
+    }
 }
