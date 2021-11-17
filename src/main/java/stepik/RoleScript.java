527cb1368b21b92cdde2960d92337b8a03f8db46
Osttanya <ostania.tania@gmail.com>
2021-11-17 23:37:56 +0200

    распределение ролей и текста по порядку

---
+++ src/main/java/stepik/RoleScript.java
@@ -0,0 +1,36 @@
+package stepik;
+
+public class RoleScript {
+
+    private String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder builder = new StringBuilder(); //создаем новую строку
+        for (int i = 0; i < roles.length; i++) { //проходим по массиву ролей
+            builder.append(roles[i]).append(":\n"); //после каждой роли ставим новую строку и двоеточие
+            for (int j = 0; j < textLines.length; j++) { //проходим по массиву такста
+                if (textLines[j].startsWith(roles[i].concat(":"))) { //если текст начинаеться с роли то обединяем их на двоеточии
+                    builder.append(j + 1).append(") ").append(textLines[j].substring(textLines[j].indexOf(":") + 2)) //прохорашиваем вид и нумерацию строк
+                            .append("\n");
+                }
+            }
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        String [] roles = {
+                "Городничий","Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String [] textLines = { "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+
+        System.out.println(new RoleScript().printTextPerRole(roles, textLines));;
+    }
+}
